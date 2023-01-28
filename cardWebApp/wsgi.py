34d21ce7979ff80d638a1efc0eee9b07b3ac98e5
srujan <srujank0903@gmail.com>
2023-01-28 19:21:36 +0530

    sd

--- cardWebApp/wsgi.py
+++ cardWebApp/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-sys.path.append('/var/www/cardWebApp/cardWebApp')
+sys.path.append('/var/www/cardWebApp')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cardWebApp.settings")
 
 application = get_wsgi_application()
