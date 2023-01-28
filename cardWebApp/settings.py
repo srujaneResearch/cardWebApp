de947f7bc990617f73a2c5997bd642193850bdb8
srujan <srujank0903@gmail.com>
2023-01-28 19:22:16 +0530

    as

--- cardWebApp/settings.py
+++ cardWebApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^tlzt*$o$jpq-06g4gig-l@h(_v74mr%*zp!!vp*pyzlvyn3zl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
